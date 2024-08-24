8d35dcf713816acd8f08bdd0a5560ed798280e87
WeyNiDrop <1179251654@qq.com>
2024-08-24 11:30:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "请设置密码："
 read passwd
 
 # 要检查的文件夹路径
-folder_path="~/shadowsocksR/shadowsocks"
+folder_path="/root/shadowsocksR/shadowsocks"
 
 # 使用 -d 判断文件夹是否存在
 if [ -d "$folder_path" ]; then
@@ -12,14 +12,14 @@ else
     echo "----install----"
     apt-get update
     apt-get install -y git
-    apt-get install -y python
+    apt-get install -y python2
     apt-get install -y screen
-    screen -S ss
     cd ~
     git clone https://github.com/WeyNiDrop/shadowsocksR.git
     
     echo "----install success----"
 fi
 
-cd ~/shadowsocksR/shadowsocks
-python server.py -p 443 -k $passwd -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible
+cd /root/shadowsocksR/shadowsocks
+#screen -S ss
+python2 server.py -p 443 -k $passwd -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible
